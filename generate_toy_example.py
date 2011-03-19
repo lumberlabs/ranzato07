1e0c859da9a5a62c99370d37df66432c9f468b57
Joshua Bleecher Snyder <josh@treelinelabs.com>
2011-03-19 09:52:49 -0700

    rename variable for clarity

--- generate_toy_example.py
+++ generate_toy_example.py
@@ -23,18 +23,18 @@ HALF_LINE_LENGTH = (LINE_LENGTH - 1) // 2
 CENTERED_ONE = numpy.zeros(LINE_LENGTH, dtype=numpy.float32)
 CENTERED_ONE[HALF_LINE_LENGTH] = 1.0
 
-LINES = [numpy.eye(LINE_LENGTH, dtype=numpy.float32), # UL/BR diagonal
-         numpy.rot90(numpy.eye(LINE_LENGTH, dtype=numpy.float32)), # BL/TR diagonal
-         numpy.outer(CENTERED_ONE, numpy.ones(LINE_LENGTH, dtype=numpy.float32)), # horizontal, centered
-         numpy.outer(numpy.ones(LINE_LENGTH, dtype=numpy.float32), CENTERED_ONE), # verical, centered
-        ]
+LINE_TYPES = [numpy.eye(LINE_LENGTH, dtype=numpy.float32), # UL/BR diagonal
+              numpy.rot90(numpy.eye(LINE_LENGTH, dtype=numpy.float32)), # BL/TR diagonal
+              numpy.outer(CENTERED_ONE, numpy.ones(LINE_LENGTH, dtype=numpy.float32)), # horizontal, centered
+              numpy.outer(numpy.ones(LINE_LENGTH, dtype=numpy.float32), CENTERED_ONE), # verical, centered
+             ]
 
 def generate_sample_patch():
     patch = numpy.zeros((PATCH_SIZE, PATCH_SIZE), dtype=numpy.float32)
 
-    distinct_lines = random.sample(LINES, NUM_LINES_PER_PATCH)
+    lines = random.sample(LINE_TYPES, NUM_LINES_PER_PATCH)
 
-    for line in distinct_lines:
+    for line in lines:
         line_center = [random.randint(LINE_PERIPHERY_SIZE, LINE_PERIPHERY_SIZE + LINE_CENTER_SIZE) for dim in xrange(2)]
         patch_area_slices = [slice(dim_center - HALF_LINE_LENGTH, dim_center + HALF_LINE_LENGTH + 1) for dim_center in line_center]
         patch[patch_area_slices] = numpy.maximum(patch[patch_area_slices], line)
